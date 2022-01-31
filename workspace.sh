d33004dfe2f763d61de0aa95853a21f07bbaf6b5
emancipat3r <ande2541@gmail.com>
2022-01-31 12:46:16 -0500

    Update workspace.sh - v 1.2

--- workspace.sh
+++ workspace.sh
@@ -30,11 +30,20 @@ then
   tmux send-keys -t 1:VPN 'vpn' ENTER 
   tmux send-keys -t 1:T1 'nmap -sC -sV $target -p- -oN recon/nmap_tcp_full.txt' 
 
+else
+
+  ## Create Tmux session and windows
+  tmux new -d -s 1
+  tmux rename-window VPN
+  tmux new-window -d -n T1
+  tmux new-window -d -n T2
+  tmux new-window -d -n T3
+  tmux new-window -d -n T4
+
+  ## Startup Commands
+  tmux send-keys -t 1:VPN 'vpn' ENTER 
+
   ## Attach session
   tmux a -t 1
-
-else
-  echo "Usage: ./workspace.sh <IP> <Name_of_Machine>"
-  echo "Example: ./workspace.sh 10.11.1.180 180_SERVMON"
 
 fi
