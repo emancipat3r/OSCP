af2965144fe760708b4959948deca633edeed5c6
emancipat3r <ande2541@gmail.com>
2022-03-03 21:56:33 -0500

    Add files via upload

---
+++ init_scans.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+	printf "USAGE: ./init_scans.sh <IP>"
+else
+	IP=$1
+
+	echo "[*] Running TCP quick scan"
+
+	ports=`nmap --min-rate 10000 $IP -p- -Pn -oN nmap_tcp_all_quick.txt | awk -F "/" '{if ($1 ~ /^[:0-9:]/) print $1}' | paste -d, -s -`
+	printf "\n[*] $(echo $IP) - OPEN PORTS\n"
+	echo "------------------------------"
+	printf "\t[-] $ports\n\n\n"
+	
+	echo "[*] Running TCP -A scan"
+	printf "\n+-------------------------------+\n"
+	nmap -A -T4 $IP -p $ports -Pn -oN nmap_tcp_all.txt
+	printf "\n+-------------------------------+\n\n\n"
+
+	echo "[*] Running UDP -sC scan"
+	printf "\n+-------------------------------+\n"
+	sudo nmap -sSUC $IP -Pn -oN nmap_udp_quick.txt
+	printf "\n+-------------------------------+\n"
+fi
+
