62503cc100aafc373a1ce9ea2e31422a73e640d8
emancipat3r <ande2541@gmail.com>
2022-03-05 20:20:50 -0500

    regular push

--- init_scans.sh
+++ init_scans.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-if [ -z "$1" ]
+if [ "$EUID" -ne 0 ]
 then
-	printf "USAGE: ./init_scans.sh <IP>"
+    echo "Please run as root"
+    exit
 else
-	IP=$1
+    if [ -z "$1" ]
+    then
+	    printf "USAGE: ./init_scans.sh <IP>"
+    else
+        IP=$1
 
-	echo "[*] Running TCP quick scan"
+	    echo "[*] Running TCP quick scan"
 
-	ports=`nmap --min-rate 10000 $IP -p- -Pn -oN nmap_tcp_all_quick.txt | awk -F "/" '{if ($1 ~ /^[:0-9:]/) print $1}' | paste -d, -s -`
-	printf "\n[*] $(echo $IP) - OPEN PORTS\n"
-	echo "------------------------------"
-	printf "\t[-] $ports\n\n\n"
-	
-	echo "[*] Running TCP -A scan"
-	printf "\n+-------------------------------+\n"
-	nmap -A -T4 $IP -p $ports -Pn -oN nmap_tcp_all.txt
-	printf "\n+-------------------------------+\n\n\n"
+    	ports=`nmap --min-rate 10000 $IP -p- -Pn -oN nmap_tcp_all_quick.txt | awk -F "/" '{if ($1 ~ /^[:0-9:]/) print $1}' | paste -d, -s -`
+    	printf "\n[*] $(echo $IP) - OPEN PORTS\n"
+    	echo "------------------------------"
+	    printf "\t[-] $ports\n\n\n"
 
-	echo "[*] Running UDP -sC scan"
-	printf "\n+-------------------------------+\n"
-	sudo nmap -sSUC $IP -Pn -oN nmap_udp_quick.txt
-	printf "\n+-------------------------------+\n"
+    	echo "[*] Running TCP -A scan"
+    	printf "\n+-------------------------------+\n"
+    	nmap -A -T4 $IP -p $ports -Pn -oN nmap_tcp_all.txt
+    	printf "\n+-------------------------------+\n\n\n"
+
+    	echo "[*] Running UDP -sC scan"
+    	printf "\n+-------------------------------+\n"
+    	sudo nmap -sUV --min-hostgroup 100 $IP -p 1-10000 -oN nmap_udp_quick.txt
+    	printf "\n+-------------------------------+\n"
+    fi
 fi
-
