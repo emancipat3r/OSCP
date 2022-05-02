fd5cb8f66004ae971279de9763b9324cd7ea6faa
emancipat3r <ande2541@gmail.com>
2022-05-01 23:32:06 -0700

    push

--- assembler.sh
+++ assembler.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 # Purpose of this script is to quickly make elf64 binaries from assembly sorce code.
-# If -g is provided as the second positional argument then the binary will be loaded 
+# If -g is provided as the second positional argument then the binary will be loaded
 # into gdb for analysis
 
 fileName="${1%%.*}" # remove .s extension
 
-nasm -f elf64 ${fileName}".s"
-ld ${fileName}".o" -o ${fileName}
-if (($#==2)); then
-        if (($2=="-g")); then 
-                gdb -q ${fileName} || ./${fileName}
-        fi
+if [ -z "$2" ]; then
+	nasm -f elf64 ${fileName}".s"
+	ld ${fileName}".o" -o ${fileName}
+fi
+
+if [ $2 == '-g' ]; then
+	gdb -q ${fileName} || ./${fileName}
+fi
+
+if [ $2 == '-d'  ]; then
+	nasm -f elf64 ${fileName}".s"
+	ld ${fileName}".o" -o ${fileName} -lc --dynamic-linker /lib64/ld-linux-x86-64.so.2
 fi
