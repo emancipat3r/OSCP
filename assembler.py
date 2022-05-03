6673c1a98e955c464147875f7f5c60be51b6a42e
emancipat3r <ande2541@gmail.com>
2022-05-02 20:55:53 -0700

    push

---
+++ assembler.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+
+import sys, os, stat
+from pwn import *
+
+context(os="linux", arch="amd64", log_level="error")
+
+ELF.from_bytes(unhex(sys.argv[1])).save(sys.argv[2])
+os.chmod(sys.argv[2], stat.S_IEXEC)
---
+++ loader.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+
+import sys
+from pwn import *
+
+context(os="linux", arch="amd64", log_level="error")
+
+run_shellcode(unhex(sys.argv[1])).interactive()
